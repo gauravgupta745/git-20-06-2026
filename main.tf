30d1bc85c4c2ac4fc2d5203bf695f226f34baed9
gauravgupta745 <gaurav.guptagn4@gmail.com>
2026-06-20 01:08:54 +0530

    add main tf file

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "akku1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "akku2"
+  location = "West Europe"
+}
